efe418e259e02e811a6b943ed322de6ce3c64abc
JongGak <121214538+JongGakz@users.noreply.github.com>
2023-03-31 17:13:10 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,10 +1,7 @@
 function solution(rsp) {
-    let split = rsp.split("");
-
-    for (let i = 0 ; i< rsp.length; i++) {
-        if (split[i] === "2") split[i] = "0";
-        else if (split[i] === "5") split[i] = "2";
-        else split[i] =  "5"
-    }
-    return split.join("");
+    return rsp.split("").map((n) => {
+        if(n === "2") {return "0"}
+        else if (n === "0") {return "5";}
+        else {return "2"}
+    }).join("");
 }
