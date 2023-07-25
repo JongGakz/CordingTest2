1b3ee127fe65cbab39f4d2da44bbf4ca8b5696f3
JongGak <121214538+JongGakz@users.noreply.github.com>
2023-07-25 14:55:38 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.34 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+      public string solution(string my_string, string letter)
+            {
+
+                string answer = "";
+                for (int i = 0; i < my_string.Length; i++)
+                {
+                    if (!my_string[i].ToString().Equals(letter))
+                    {
+                        answer += my_string[i];
+                    } 
+                }
+                
+                return answer;
+            }
+}
