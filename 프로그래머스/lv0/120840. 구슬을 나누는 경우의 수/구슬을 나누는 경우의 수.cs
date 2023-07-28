31ad3e16b8219a1a23bf11a1604240d156937acc
JongGak <121214538+JongGakz@users.noreply.github.com>
2023-07-28 10:52:39 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.29 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cs
@@ -1,21 +1,29 @@
 using System;
 
 public class Solution {
-    public long solution(int balls, int share)
+      public double solution(int balls, int share)
             {
-                // Math.Min()은 둘중 더 작은 값을 반환함
-                share = Math.Min(balls - share, share);
 
-                // 재귀함수를 돌면서 share가 0이면 1을 리턴
-                if (share == 0)
-                    return 1;
+                double a = 1;
+                double b = 1;
+                double c = 1;
+                double d = balls - share;
 
-                // 재귀함수를 통해 balls와 share를 1씩 줄임
-                long answer = solution(balls - 1, share - 1);
-                answer *= balls;
-                answer /= share;
+                for (double i = 1; i <= balls; i++)
+                {
+                    a *= i;
+                }
+                for (double i = 1; i <= share; i++)
+                {
+                    b *= i;
+                }
+                for (double i = 1; i <= d; i++)
+                {
+                    c *= i;
+                }
 
-                // 한줄 코드 return share == 0 ? 1 : solution(balls - 1, share -1) * balls / share;
-                return answer;
+
+                 return Math.Round(a / (b * c));
             }
+
 }
