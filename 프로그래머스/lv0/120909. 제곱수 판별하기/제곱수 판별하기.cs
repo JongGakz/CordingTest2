42a06ddb547696e1005de01e4b61f8749442cd9c
JongGak <121214538+JongGakz@users.noreply.github.com>
2023-07-28 16:09:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.37 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+   public int solution(int n)
+            {
+                int answer = 0;
+                if (Math.Sqrt(n) % 1 == 0)
+                    return 1;
+                else
+                    return 2;
+            }
+}
