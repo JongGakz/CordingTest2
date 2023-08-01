dff1fe3eedae77f6f96488c09e792bf98b7c9c90
JongGak <121214538+JongGakz@users.noreply.github.com>
2023-08-01 10:38:59 +0900

    [level 0] Title: 중복된 문자 제거, Time: 2.87 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120888. 중복된 문자 제거/중복된 문자 제거.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+public class Solution {
+    public string solution(string my_string)
+        {
+            string answer = "";
+            IEnumerable<char> enumerable = my_string.Distinct();
+
+            List<char> list = enumerable.ToList();
+
+            foreach (char c in list)
+            {
+                answer += c;
+            }
+            
+            return answer;
+            ;
+
+        }
+}
