4f34f718274f44bd81a7be57c902aa95e64fd610
JongGak <121214538+JongGakz@users.noreply.github.com>
2023-08-02 16:15:03 +0900

    [level 0] Title: 외계어 사전, Time: 0.56 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.cs
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.cs
@@ -3,25 +3,27 @@ using System;
 public class Solution {
      public int solution(string[] spell, string[] dic)
         {
-            int answer = 2;
-            for (int i = 0; i < dic.Length; i++)
+              int answer = 0;
+        for(int i =0 ; i < dic.Length;i++)
+        {
+            for(int k =0 ; k < spell.Length;k++)
             {
-                for (int j = 0; j < spell.Length; j++) // Fixed the loop index from i to j
+                if(dic[i].Contains(spell[k]))
                 {
-                    if (!(dic[i].Contains(spell[j])))
-                    {
-                        break;
-                    }
-                    dic[i] = dic[i].Replace(spell[j], "");
+                    dic[i] = dic[i].Replace(spell[k],String.Empty);
 
-                    if (dic[i].Equals("") && j == spell.Length - 1)
+                    if(dic[i] == String.Empty && k == spell.Length-1)
                     {
-                        answer = 1; // Set answer to 1 if the condition is met
-                        break; // We can break here since we already know the answer is 1
+                        return 1;
                     }
                 }
+                else
+                {
+                   break;
+                }
             }
-            return answer;
+        }
+        return 2;
         }
 
 }
